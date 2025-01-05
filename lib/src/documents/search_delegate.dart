d805a0de235d168757c1eae15d1c7f8de31be569
Ahmed Hamza <hdamnit@gmail.com>
2025-01-06 01:19:49 +0530

    fix: Add missing import for DocumentDetailView in search_delegate.dart

--- lib/src/documents/search_delegate.dart
+++ lib/src/documents/search_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:law_india/src/documents/document_detail_view.dart';
 import '../bloc/document/document_bloc.dart';
 import '../data/models/document_model.dart';
 
