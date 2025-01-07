f81f6c92cbe437e05533edc17ed12ade6d42be80
Your Name <you@example.com>
2025-01-07 21:05:03 +0530

    refactor: Update imports in document_list_view.dart

--- lib/src/documents/document_list_view.dart
+++ lib/src/documents/document_list_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:law_india/src/documents/search_delegate.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../search/document_search_delegate.dart';
 import '../settings/reading_settings.dart';
-import '../data/models/document_model.dart';
 import '../bloc/document/document_bloc.dart';
 import 'document_detail_view.dart';
 
