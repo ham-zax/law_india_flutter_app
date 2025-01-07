a6797eedad5cb3404c4576e0b15c8e64f6eb4188
Your Name <you@example.com>
2025-01-08 00:10:56 +0530

    refactor: Simplify imports in document_detail_view.dart

--- lib/src/documents/document_detail_view.dart
+++ lib/src/documents/document_detail_view.dart
@@ -1,5 +1,5 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' show SharedAxisTransition, SharedAxisTransitionType;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import '../bloc/document/document_bloc.dart';
